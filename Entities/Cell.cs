779754b67a08fa7068b30f8a1fe13898a5569044
Delcio Torres <delcio.at@gmail.com>
2021-01-03 16:08:48 -0300

    add Cell entity to Form1

---
+++ Entities/Cell.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using Xadrez.Properties;
+
+namespace Xadrez.Entities {
+   class Cell : PictureBox {
+      private static Image BLACK = Resources.square_black;
+      private static Image WHITE = Resources.square_white;
+
+      public Position Position { get; set; }
+
+      public Cell(Position position) {
+         this.Image = position.isEven() ? BLACK : WHITE;
+         this.Location = position.Location;
+         this.Width = Position.CELL_SIZE;
+         this.Height = Position.CELL_SIZE;
+         this.SizeMode = PictureBoxSizeMode.StretchImage;
+      }
+   }
+}
---
+++ Entities/Position.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace Xadrez.Entities {
+   class Position {
+      public static int CELL_SIZE = 56;
+
+      public Point Location { get; set; }
+      public int X { get; set; }
+      public int Y { get; set; }
+
+      public Position(int x, int y) {
+         Location = new Point(x * CELL_SIZE, (7 - y) * CELL_SIZE);
+         Y = y;
+         X = x;
+      }
+
+      public bool isEven() {
+         return X + Y % 2 == 0;
+      }
+   }
+}
