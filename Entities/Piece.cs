00a0343f356ab8a0dd0be71d131791d676ac2eff
Delcio Torres <delcio.at@gmail.com>
2021-01-03 18:49:20 -0300

    add Piece abstract class

---
+++ Entities/Piece.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Xadrez.Entities {
+   abstract class Piece : PictureBox {
+
+      public Cell FirstCell { get; set; }
+      public Cell Cell { get; set; }
+      public Color Color { get; set; }
+
+      public Piece(Cell firstCell, Color color) {
+         FirstCell = firstCell;
+         Color = color;
+
+         this.Location = firstCell.Location;
+         this.Width = Position.CELL_SIZE;
+         this.Height = Position.CELL_SIZE;
+         this.SizeMode = PictureBoxSizeMode.StretchImage;
+         this.Image = setImage();
+      }
+
+      public void resetPosition() {
+         Cell = FirstCell;
+      }
+
+      protected abstract Image setImage();
+   }
+}
