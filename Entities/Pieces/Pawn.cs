1a7371bab3d6028af75b137267e554f8b2441d75
Delcio Torres <delcio.at@gmail.com>
2021-01-03 18:49:37 -0300

    add Pieces Pawn and Rook

---
+++ Entities/Pieces/Pawn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xadrez.Properties;
+
+namespace Xadrez.Entities.Pieces {
+   class Pawn : Piece {
+      public Pawn(Cell firstCell, Color color) : base(firstCell, color) {
+
+      }
+
+      protected override Image setImage() {
+         if (Color == Color.BLACK) {
+            return Resources.piece_black_pawn;
+         }
+         return Resources.piece_white_pawn;
+      }
+   }
+}
---
+++ Entities/Pieces/Rook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xadrez.Properties;
+
+namespace Xadrez.Entities.Pieces {
+   class Rook : Piece {
+      public Rook(Cell firstCell, Color color) : base(firstCell, color) {
+
+      }
+
+      protected override Image setImage() {
+         if (Color == Color.BLACK) {
+            return Resources.piece_black_rook;
+         }
+         return Resources.piece_white_rook;
+      }
+   }
+}
